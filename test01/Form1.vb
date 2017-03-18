4149af5bb62d2b95d0ad5040d0a3ed976d06930a
Dreamess <bird91322@gmail.com>
2017-03-18 20:30:14 +0800

    Add files via upload

---
+++ test01/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim id As Single
+        id = InputBox("請輸入您喜歡的號碼(0~9)", "輸入顯示")
+        MsgBox("你的幸運號碼是:" & id & vbCrLf & "good luck to you.")
+    End Sub
+End Class
